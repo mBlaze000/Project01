9cfe5a438b227f8e16dae88f622e784013e684ee
James Burke <jamesburke@g.harvard.edu>
2014-06-30 08:49:34 -0400

    Updated URL

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+    <title>P1 - James Burke</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <!-- Bootstrap core CSS -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+    
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  
+  <body id="home">
+  
+    <div class="container">
+
+      <h1>James Burke</h1>
+        <img src="images/jb.jpg" class="img-responsive" alt="James Burke" title="James Burke">
+        <h2>In 2+ sentences describe who you are and what you do.</h2>
+      <p class="lead">I am a freelance video professional. I started making short films in grade school and have been <a href="http://youtu.be/WkKYyxCZ8QU">producing and editing video</a> my entire career. Secondarily, I have been a web designer and developer either internally with different companies or as a consultant.</p>
+        <h2>Why are you taking this course?</h2>
+      <p class="lead">After working as a web designer for different corporations, I thought it might be time to get some formal training. Most of what I know about web design and development is from trial and error. I'm hoping that a formal background will give me a much deeper understanding of the technology and processes.
+        <h2>What previous experience do you have with HTML/CSS?</h2>
+      <p class="lead">I have been doing front-end web design for corporations on and off, for almost 20 years. 
+        <h2>What previous experience do you have with programming?</h2>
+      <p class="lead">I ran a small multimedia company for a few years and in that time was doing a lot of interactive CD-ROM development. This meant programming in a language called Lingo for development in Adobe (Macromedia) Director and ActionScript in Flash. I have done some tinkering in Classic ASP as well.</p>
+        <h2>Have you worked with PHP before? If so, to what extent?</h2>
+      <p class="lead">The first course I took at the Harvard Extension School was <strong>CSCI E-12: Fundamentals of Website Development</strong>. In that class we did some work in PHP. This was my first hands-on exposure to PHP. It reminded me a little of Classic ASP. In the class we used PHP includes and basic variables. For my final project, I wrote some PHP code to determine the root of the website and generate a popcorn trail with links relative to the root.</p>
+        <h2>Describe your level of experience with programming.</h2>
+      <p class="lead">In college I took a class in PASCAL and enjoyed it. I have done some type of programming ever since.</p>
+        <h2>Are you working on a Mac, PC, or some other platform?</h2>
+      <p class="lead">I am currently working on a Mac computer.</p>
+        <h2>Anything else we should know?</h2>
+      <p class="lead">I write a weekly blog on <a href="http://blog.dvfanatics.com">video production</a> and I have a great Golden Retriever named Max.</p>
+      <p><img src="images/max.jpg" class="img-responsive" alt="Max the dog" title="Max the dog"></p>
+
+  </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+    
+</body>
+</html>
